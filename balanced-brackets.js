9cb5f44884945849fceeef75b18a1bdad1ee97d9
Patrick Slagle <slaglebagel17@gmail.com>
2018-10-30 13:55:31 -0700

    completed balanced-brackets

---
+++ balanced-brackets.js
@@ -0,0 +1,17 @@
+function balancedBrackets(brackets) {
+  const bracketStack = [];
+  const oppositeBrackets = {
+    '[': ']',
+    '{': '}',
+    '(': ')'
+  }
+  for (let i = 0; i < brackets.length; i += 1) {
+    if (brackets[i] in oppositeBrackets) bracketStack.push(brackets[i]);
+    else if (brackets[i] === ']' || brackets[i] === '}' || brackets[i] === ')') {
+      if (brackets[i] !== oppositeBrackets[bracketStack.pop()]) return false; 
+    }
+  }
+  return bracketStack.length === 0; 
+}
+
+console.log(balancedBrackets('[{[]})]a()'))
