5adbc214c406c2aa9c9ab34ded1a46d07e05ed5c
Patrick Slagle <slaglebagel17@gmail.com>
2018-10-31 19:59:15 -0700

    complted in-order-traversal

---
+++ in-order-traversal.js
@@ -0,0 +1,34 @@
+function Tree(value) {
+  this.left = null;
+  this.right = null; 
+  this.value = value; 
+}
+
+const tree = new Tree(10);
+tree.left = new Tree(6);
+tree.left.left = new Tree(3);
+tree.left.right = new Tree(8);
+tree.left.right.left = new Tree(7);
+
+tree.right = new Tree(14);
+tree.right.left = new Tree(12);
+tree.right.right = new Tree(15);
+tree.right.left.right = new Tree(13);
+tree.right.left.right.right = new Tree(80)
+
+function inOrderTraversal(tree) {
+  if (tree.left) inOrderTraversal(tree.left)
+  console.log(tree.value);
+  if (tree.right) inOrderTraversal(tree.right);
+}
+
+// inOrderTraversal(tree);
+
+const temp = {}
+temp.hi = tree.left.left
+tree.left.left = tree.left.right; 
+tree.left.left.value = 2; 
+console.log(tree.left.left)
+// tree.left.right = temp; 
+// console.log(tree.left)
+console.log(temp)
