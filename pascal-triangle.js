42f1ad15a96b8ba4f1bd150a5ff71048e857133a
Patrick Slagle <slaglebagel17@gmail.com>
2019-01-13 13:57:55 -0500

    finished pascal-triangle

--- pascal-triangle.js
+++ pascal-triangle.js
@@ -31,3 +31,23 @@
     [1,5,10,10,5,1]
   ]
 */
+
+const pascalTriangle = (num) => {
+  const tri = [];
+  for (let i = 0; i < num; i += 1) {
+    if (i === 0) tri.push([1]);
+    else if (i === 1) tri.push([1, 1]);
+    else {
+      const row = [1];
+      const lastRow = tri[tri.length - 1];
+      for (let j = 0; j < lastRow.length - 1; j += 1) {
+        row.push(lastRow[j] + lastRow[j + 1]);
+      }
+      row.push(1);
+      tri.push(row);
+    }
+  }
+  return tri;
+};
+
+console.log(pascalTriangle(6));
